1d9984f297c3193ef5e13be9cfbe6720db011640
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-12-06 13:26:53 +0300

    added 'payout' heading to BettingPayouts

--- src/components/Bets/Bets.js
+++ src/components/Bets/Bets.js
@@ -1,56 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Heading, Select, VStack } from '@chakra-ui/react'
 import BettingPayouts from './BettingPayouts'
 import MyCars from '../miscellaneous/MyCars'
 import BetDisplay from './BetDisplay'
 
 function Bets({ bets, onBetColorChange, cars, activeBetIndex, setActiveBetIndex }) {
-	const handleColorChange = (title, newColor) => {
-		onBetColorChange(title, newColor)
+  const handleColorChange = (title, newColor) => {
+    onBetColorChange(title, newColor)
 
-		// Automatically move to the next bet when a color is selected
-		const betIndex = bets.findIndex((bet) => bet.title === title)
-		if (betIndex === bets.length - 1) {
-			// If it's the last bet, do not show any more bets.
-			setActiveBetIndex(bets.length)
-		} else if (betIndex >= 0 && betIndex < bets.length - 1) {
-			// Move to the next bet
-			setActiveBetIndex(betIndex + 1)
-		}
-	}
+    // Automatically move to the next bet when a color is selected
+    const betIndex = bets.findIndex((bet) => bet.title === title)
+    if (betIndex === bets.length - 1) {
+      // If it's the last bet, do not show any more bets.
+      setActiveBetIndex(bets.length)
+    } else if (betIndex >= 0 && betIndex < bets.length - 1) {
+      // Move to the next bet
+      setActiveBetIndex(betIndex + 1)
+    }
+  }
 
-	return (
-		<VStack w={'full'} spacing={8}>
-			<MyCars cars={cars} />
-			<BetDisplay bets={bets} />
+  return (
+    <VStack w={'full'} spacing={8}>
+      <MyCars cars={cars} />
+      <BetDisplay bets={bets} />
 
-			{bets.map(({ number, color, title, payout }, betIndex) => (
-				<React.Fragment key={number}>
-					{activeBetIndex === betIndex && (
-						<VStack w={'full'}>
-							<Heading>{title}</Heading>
-							<BettingPayouts payout={payout} color={color} />
-							<Select
-								onChange={(e) => handleColorChange(title, e.target.value)}
-								value={color}
-								placeholder='Select Car'
-								colorScheme={'gray'}
-								border={'3px solid'}
-								borderColor={'gray.600'}>
-								{cars.map((car) => (
-									<option key={car.name} value={car.color}>
-										{car.name.toUpperCase()}
-									</option>
-								))}
-							</Select>
-						</VStack>
-					)}
-				</React.Fragment>
-			))}
+      {bets.map(({ number, color, title, payout }, betIndex) => (
+        <React.Fragment key={number}>
+          {activeBetIndex === betIndex && (
+            <VStack w={'full'}>
+              <Heading>{title}</Heading>
+              <BettingPayouts payout={payout} color={color} />
+              <Select
+                onChange={(e) => handleColorChange(title, e.target.value)}
+                value={color}
+                placeholder='Select Car'
+                colorScheme={'gray'}
+                border={'3px solid'}
+                borderColor={'gray.600'}>
+                {cars.map((car) => (
+                  <option key={car.name} value={car.color}>
+                    {car.name.toUpperCase()}
+                  </option>
+                ))}
+              </Select>
+            </VStack>
+          )}
+        </React.Fragment>
+      ))}
 
-			{activeBetIndex >= bets.length && <Heading>All bets are set!</Heading>}
-		</VStack>
-	)
+      {activeBetIndex >= bets.length && <Heading>All bets are set!</Heading>}
+    </VStack>
+  )
 }
 
 export default Bets
--- src/components/Bets/BettingPayouts.js
+++ src/components/Bets/BettingPayouts.js
@@ -1,34 +1,35 @@
 import React from 'react'
-import { Box, Text, HStack, VStack } from '@chakra-ui/react'
+import { Box, Text, HStack, VStack, Heading } from '@chakra-ui/react'
 
 const splitOrdinalNumber = (number) => {
-	const ordinals = { 1: 'st', 2: 'nd', 3: 'rd' }
-	return { number, suffix: ordinals[number] }
+  const ordinals = { 1: 'st', 2: 'nd', 3: 'rd' }
+  return { number, suffix: ordinals[number] }
 }
 
 function BettingPayouts({ payout }) {
-	return (
-		<HStack w={'full'} p={2} border={'3px solid'} borderColor={'gray.600'} borderRadius={'lg'}>
-			<VStack w={'full'}>
-				<HStack w={'full'} justify={'space-evenly'}>
-					{Object.entries(payout).map(([position, amount]) => {
-						const { number, suffix } = splitOrdinalNumber(position)
-						return (
-							<Box key={position} textAlign='center'>
-								<Text>
-									{number}
-									<Text as='sup' fontSize='sm'>
-										{suffix}
-									</Text>
-								</Text>
-								<Text>${amount} M</Text>
-							</Box>
-						)
-					})}
-				</HStack>
-			</VStack>
-		</HStack>
-	)
+  return (
+    <HStack w={'full'} p={2} border={'3px solid'} borderColor={'gray.600'} borderRadius={'lg'}>
+      <VStack w={'full'}>
+        <Heading size={'sm'}>Payouts</Heading>
+        <HStack w={'full'} justify={'space-evenly'}>
+          {Object.entries(payout).map(([position, amount]) => {
+            const { number, suffix } = splitOrdinalNumber(position)
+            return (
+              <Box key={position} textAlign='center'>
+                <Text>
+                  {number}
+                  <Text as='sup' fontSize='sm'>
+                    {suffix}
+                  </Text>
+                </Text>
+                <Text>${amount} M</Text>
+              </Box>
+            )
+          })}
+        </HStack>
+      </VStack>
+    </HStack>
+  )
 }
 
 export default BettingPayouts
