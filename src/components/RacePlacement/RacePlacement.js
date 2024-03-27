966a8827fe9b4456cc212dec5ab242e7bf68474f
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2024-03-27 16:09:46 +0300

    Changed attribues of reset placment button in RacePlacement

--- src/components/RacePlacement/RacePlacement.js
+++ src/components/RacePlacement/RacePlacement.js
@@ -4,72 +4,71 @@ import Car from '../miscellaneous/Car'
 import CarsWrapper from '../miscellaneous/CarsWrapper'
 
 function RacePlacement({ placements, onPlacementColorChange, cars }) {
-	// Function to find a car by its color
-	function findCarbyColor(color) {
-		return cars.find((car) => car.color === color)
-	}
+  // Function to find a car by its color
+  function findCarbyColor(color) {
+    return cars.find((car) => car.color === color)
+  }
 
-	function getAvailableCars(currentColor) {
-		const selectedColors = placements.map((p) => p.color)
-		return cars.filter((car) => !selectedColors.includes(car.color) || car.color === currentColor)
-	}
+  function getAvailableCars(currentColor) {
+    const selectedColors = placements.map((p) => p.color)
+    return cars.filter((car) => !selectedColors.includes(car.color) || car.color === currentColor)
+  }
 
-	function resetAllColors() {
-		placements.forEach((placement) => onPlacementColorChange(placement.title, ''))
-	}
+  function resetAllColors() {
+    placements.forEach((placement) => onPlacementColorChange(placement.title, ''))
+  }
 
-	// Function to split number and suffix
-	function splitNumberAndSuffix(pos) {
-		const suffixes = ['th', 'st', 'nd', 'rd']
-		const v = pos % 100
-		const suffix = suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]
-		return { number: pos, suffix }
-	}
+  // Function to split number and suffix
+  function splitNumberAndSuffix(pos) {
+    const suffixes = ['th', 'st', 'nd', 'rd']
+    const v = pos % 100
+    const suffix = suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]
+    return { number: pos, suffix }
+  }
 
-	return (
-		<Stack w={'full'} spacing={4}>
-			{placements.map((placement) => {
-				const availableCars = getAvailableCars(placement.color)
-				const { number, suffix } = splitNumberAndSuffix(placement.pos)
+  return (
+    <Stack w={'full'} spacing={4}>
+      {placements.map((placement) => {
+        const availableCars = getAvailableCars(placement.color)
+        const { number, suffix } = splitNumberAndSuffix(placement.pos)
 
-				return (
-					<Stack w={'full'} key={placement.title}>
-						<HStack>
-							<Text minW={'40px'}>
-								{number}
-								<Text as='sup' fontSize='sm' fontWeight='bold'>
-									{suffix}
-								</Text>
-							</Text>
-							{placement.color ? (
-								<CarsWrapper>
-									<Car name={findCarbyColor(placement.color)?.name} color={placement.color} />
-								</CarsWrapper>
-							) : (
-								<Select
-									onChange={(e) => onPlacementColorChange(placement.title, e.target.value)}
-									value={placement.color}
-									placeholder='Select Car'
-									colorScheme={'gray'}
-									border={'3px solid'}
-									borderColor={'gray.600'}>
-									{availableCars.map((car) => (
-										<option key={car.color} value={car.color}>
-											{car.name.toUpperCase()}
-										</option>
-									))}
-								</Select>
-							)}
-						</HStack>
-					</Stack>
-				)
-			})}
-
-			<Button colorScheme='orange' onClick={resetAllColors}>
-				Reset Placements
-			</Button>
-		</Stack>
-	)
+        return (
+          <Stack w={'full'} key={placement.title}>
+            <HStack>
+              <Text minW={'40px'}>
+                {number}
+                <Text as='sup' fontSize='sm' fontWeight='bold'>
+                  {suffix}
+                </Text>
+              </Text>
+              {placement.color ? (
+                <CarsWrapper>
+                  <Car name={findCarbyColor(placement.color)?.name} color={placement.color} />
+                </CarsWrapper>
+              ) : (
+                <Select
+                  onChange={(e) => onPlacementColorChange(placement.title, e.target.value)}
+                  value={placement.color}
+                  placeholder='Select Car'
+                  colorScheme={'gray'}
+                  border={'3px solid'}
+                  borderColor={'gray.600'}>
+                  {availableCars.map((car) => (
+                    <option key={car.color} value={car.color}>
+                      {car.name.toUpperCase()}
+                    </option>
+                  ))}
+                </Select>
+              )}
+            </HStack>
+          </Stack>
+        )
+      })}
+      <Button w='200px' alignSelf={'end'} colorScheme='red' onClick={resetAllColors}>
+        Redo Placements?
+      </Button>
+    </Stack>
+  )
 }
 
 export default RacePlacement
